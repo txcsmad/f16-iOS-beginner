5658fd72a807bc615dc88f5bc41086e7ce229158
Jesse Tipton <me@jessetipton.com>
2016-09-29 20:05:39 -0500

    workshop 3 end

---
+++ 3 - There's Always Money in the Banana Stand/Directory - End/Directory/Contact.swift
@@ -0,0 +1,18 @@
+//
+//  Contact.swift
+//  Directory
+//
+//  Created by Jesse Tipton on 9/29/16.
+//  Copyright © 2016 Matt. All rights reserved.
+//
+
+import Foundation
+
+struct Contact {
+    
+    let firstName: String
+    let lastName: String
+    let phone: String
+    let bio: String
+    
+}
