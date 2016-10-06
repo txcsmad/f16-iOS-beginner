e35b017e2cb1a1449b71ab75a1b2293ea63cbed3
Jesse Tipton <me@jessetipton.com>
2016-10-06 18:23:01 -0500

    for british eyes only

---
+++ 4 - For British Eyes Only/Directory - Start/Directory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Directory
+//
+//  Created by Matthew Ruston on 9/21/16.
+//  Copyright © 2016 Matt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
